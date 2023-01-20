cacf3b60aa41d7449a65049b49797b6660015b94
David Mora <dmorahidalgo@gmail.com>
2023-01-20 20:09:23 +0100

    Javadoc - Cartro objecte

--- Cartro.java
+++ Cartro.java
@@ -5,7 +5,9 @@
 package pt1_bingo;
 
 /**
- *
+ * Objecte de tipus Cartro. Conté dos atributs com a abstracció d'un cartro de bingo.
+ * Ambdós son un array bidimensional de nombres sencers, el primer l'original i el segon
+ * una copia sobre la que s'ha de treballar durant l'execució del Bingo.
  * @author moraman
  */
 public class Cartro {
